d3be710ff86e649fe05bc74c03065503a129fb22
Mike Ton <mike.ton@gmail.com>
2012-11-29 22:16:02 -0800

    Pretty format twitter.js

--- twitter.js
+++ twitter.js
@@ -3,7 +3,19 @@ var app     = express.createServer() ;
 var tweets = [];
 
 app.get('/', function(req, res){ //Get request. Filter based on HTTP verbs, and specific URLs
-  res.send('Welcome to Mikes Node Twitter');
+  // res.send('Welcome to Mikes Node Twitter');
+
+  var title = 'Chirpie';
+  var header = 'Welcome to Chirpie';
+
+  res.render('index', {
+    locals: {
+      'title'     : title  ,
+      'header'    : header ,
+      'tweets'    : tweets ,
+      stylesheets : ['/public/style.css']
+    }
+  });
 });
 
 app.post('/send', express.bodyParser(), function(req, res){ //middleware for authentication, logging...etc
